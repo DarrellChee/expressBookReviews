ab7d2b5a54b4311595b0e658fe1c719a5f23dd19
Darrell Chee <cheedarrell@gmail.com>
2025-04-21 17:36:51 +0700

    implemented encryption

--- router/auth_users.js
+++ router/auth_users.js
@@ -2,21 +2,48 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+const bcrypt = require('bcryptjs');
+
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = inputUsername =>{ 
+  // users.find(user => user.username == inputUsername)
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
 
+const authenticatedUser = async (username,password)=>{
+  const passwordMatcher = async (username , inputPassword) =>{
+    try{
+      if(await bcrypt.compare(inputPassword , users.find(user => user.username === username).password)){
+        return true;
+      }
+    }catch{
+      return false;
+    }
+  }
+
+  if(users.find(user => user.username === username)){
+    if(await passwordMatcher(username, password)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+}
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+
+regd_users.post("/login", async (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if(await authenticatedUser(req.body.username, req.body.password)){
+    res.status(201).json("Welcome " + req.body.username);
+  }else{
+    res.status(401).json("Username or password is incorrect");
+  }
+});
+
+regd_users.get("/login", (req,res) => {
+  res.status(201).json(users);
 });
 
 // Add a book review
--- router/general.js
+++ router/general.js
@@ -3,41 +3,73 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const bcrypt = require('bcryptjs');
 
+const encrypt = async password =>{
+  try{
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password , salt);
+    return hashedPassword;
+  }catch{
+    return false;
+  }
+}
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.post("/register", async (req,res) => {
+  const {username , password} = req.body;
+  if(!username || !password){
+    return res.status(401).json({error : "Fill in the username or password"})
+  }
+  if(users.find(user => user.username == username)){
+    return res.status(401).json({error: `Username has been taken, choose another one`});    
+  }
+  const hashed = await encrypt(password);
+  if(hashed == false){
+    return res.status(401).json({error: "Failed to encrypt password, try again later"});
+  }
+  users.push({"username" : username, "password" : hashed})
+  res.status(201).json(`Welcome ${username} your account has been successfully registered`);
+  }
+);
+
+public_users.get("/register", (req,res) => {
+  res.status(201).json(users);
+})
+
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', (req, res) => {
+  const titles = Object.values(books).map(book => book.title);
+  res.status(201).json(titles);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let target_book = Object.values(books)[(req.params.isbn) - 1];
+  res.status(201).json(target_book);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let target_book = Object.values(books).filter(e => e.author == req.params.author);
+  res.status(201).json(target_book[0]);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let target_book = Object.values(books).filter(e => e.title == req.params.title);
+  let result =   
+    "Author: " + target_book[0].author + "\n"+
+    "Title: " + target_book[0].title + "\n" +
+    "Reviews: " + target_book[0].reviews;
+    res.status(201).json(result);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let target_book = Object.values(books)[(req.params.isbn)-1];
+  res.status(201).json("Review of "+ target_book.title + " : " + target_book.reviews);
 });
 
 module.exports.general = public_users;
